3dddede15554f0892159f8ff019f546f72abecaf
Seancheey <adls371@163.com>
2016-05-17 10:33:39 +0800

    add cancel button

--- src/com/seancheey/gui/ImagePreviewFrame.java
+++ src/com/seancheey/gui/ImagePreviewFrame.java
@@ -19,7 +19,7 @@ import com.seancheey.imagegen.DataImageGen;
 public class ImagePreviewFrame extends JFrame {
 	private static final long serialVersionUID = 8202432493435137827L;
 	private DataImageGen gen;
-	private JButton genButton;
+	private JButton genButton, cancelButton;
 	private JPanel panel;
 
 	public ImagePreviewFrame(String filename, String type, DataImageGen gen) {
@@ -48,27 +48,43 @@ public class ImagePreviewFrame extends JFrame {
 				});
 				genButton.setBackground(Color.WHITE);
 			}
+			cancelButton = new JButton(LanguageConverter.defaultCvt().convertString("Cancel"));
+			{
+				cancelButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setBackground(Color.WHITE);
+			}
 			JPanel imagePanel = this.gen.getPanel();
 			GridBagLayout layout = new GridBagLayout();
 			GridBagConstraints c = new GridBagConstraints();
 			c.fill = GridBagConstraints.BOTH;
+			c.anchor = GridBagConstraints.CENTER;
 			c.gridx = 0;
 			c.gridy = 0;
-			c.gridwidth = 1;
+			c.gridwidth = 2;
 			c.gridheight = 5;
 			c.weightx = 1;
 			c.weighty = 1;
 			layout.setConstraints(imagePanel, c);
-			c.weightx = 0;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			c.weightx = 0.5;
 			c.weighty = 0;
 			c.gridx = 0;
 			c.gridy = 5;
 			c.gridwidth = 1;
 			c.gridheight = 1;
 			layout.setConstraints(genButton, c);
+			c.gridx = 1;
+			layout.setConstraints(cancelButton, c);
+
 			panel.setLayout(layout);
 			panel.add(imagePanel);
 			panel.add(genButton);
+			panel.add(cancelButton);
 		}
 		setVisible(true);
 	}
